8144ddffc9ae9125ed2107f400a986879dc5577e
Ekin Karıncalı <s230446019@stu.thk.edu.tr>
2026-03-18 16:27:36 +0300

    feat: declare rigidbody and freeze physics rotation

---
+++ Sample Git LFS/My project/Assets/Scripts/FlightController.cs
@@ -0,0 +1,44 @@
+// FlightController.cs 
+// CENG 454 – HW1: Sky-High Prototype 
+// Author: Ekin Karıncalı | Student ID: 230446019 
+ 
+using UnityEngine; 
+ 
+public class FlightController : MonoBehaviour 
+{ 
+    [SerializeField] private float pitchSpeed  = 45f;  // degrees/second 
+    [SerializeField] private float yawSpeed    = 45f;  // degrees/second 
+    [SerializeField] private float rollSpeed   = 45f;  // degrees/second 
+    [SerializeField] private float thrustSpeed = 5f;   // units/second 
+ 
+    // TODO (Task 3-A): Declare a private Rigidbody field named 'rb' 
+    private Rigidbody rb;
+ 
+    void Start() 
+    { 
+        // TODO (Task 3-B): Cache GetComponent<Rigidbody>() into 'rb'. 
+        //                  Then set rb.freezeRotation = true. 
+        //                  Why is freezeRotation needed? Answer in your PDF. 
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    } 
+ 
+    void Update()// or FixedUpdate() 
+    { 
+        HandleRotation(); 
+        HandleThrust(); 
+    } 
+ 
+    private void HandleRotation() 
+    { 
+        // TODO (Task 3-C): 
+        // Pitch   
+        // Roll    
+ 
+    } 
+ 
+    private void HandleThrust() 
+    { 
+        // TODO (Task 3-D): 
+    } 
+}
